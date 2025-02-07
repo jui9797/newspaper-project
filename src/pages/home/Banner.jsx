36a25ba6b34dcd3cc1f74ea31fd46f507949e26e
Lenovo <jannatjui414@gmail.com>
2025-02-08 00:32:28 +0600

    change article image

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -35,7 +35,7 @@ const axiosPublic =useAxiosPublic()
     
 
     return (
-        <div className='w-full h-[450px]'>
+        <div className='w-full h-[300px] md:h-[450px] lg:h-[500px]'>
             <Swiper
                 spaceBetween={30}
                 centeredSlides={true}
@@ -62,11 +62,13 @@ const axiosPublic =useAxiosPublic()
                     {
                         // console.log(article.image)
                     return <SwiperSlide key={article._id}>
-                        <div className=' w-full h-full bg-no-repeat bg-cover'
+                        <div className=' w-full h-full bg-no-repeat bg-center bg-cover'
                         style={{ backgroundImage: `url('${article.image}')` }}>
-
+                            <div className='hero-overlay bg-opacity-70 lora flex justify-center items-center'>
+                                <h2 className='hidden md:flex text-xl md:text-3xl lg:text-5xl bg-gradient-to-b from-blue-500 to-white bg-clip-text text-transparent'>{article?.title}</h2>
+                            </div>
                         </div>
-                    {/* <img src={article?.image} alt="img" className='w-full h-full object-cover' /> */}
+                    {/* <img src={article?.image} alt="img" className='w-full h-full object-contain opacity-80' /> */}
                     </SwiperSlide>
                     }
                        
